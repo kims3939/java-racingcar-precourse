61741918b61d1fcb5f7d21b13246353fc776df06
김민우 (IT BDO Group) <kim.minwoo@kt.com>
2022-04-24 19:07:03 +0900

    feat: 경주 게임은 중복된 이름의 자동차를 만들 수 없다

--- src/main/java/racingcar/domain/RacingGame.java
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -1,11 +1,23 @@
 package racingcar.domain;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RacingGame {
 
     private Lineup lineup;
 
     public Lineup setLineup(String... carNames) {
+        assertDuplicateNames(carNames);
         lineup = new Lineup(carNames);
         return lineup;
+    }
+
+    private void assertDuplicateNames(String... names) {
+        Set<String> nameSet = new HashSet<>(Arrays.asList(names));
+        if (nameSet.size() < names.length) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/racingcar/domain/RacingGameTest.java
+++ src/test/java/racingcar/domain/RacingGameTest.java
@@ -1,6 +1,7 @@
 package racingcar.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +11,25 @@ class RacingGameTest {
     @DisplayName("경주 게임은 자동차들의 이름을 전달받아서 자동차들을 만든다")
     void make_lineup() {
         //given
-        String car1 = "car1";
-        String car2 = "car2";
-
         RacingGame racingGame = new RacingGame();
 
         //when
-        Lineup lineup = racingGame.setLineup(car1, car2);
+        Lineup lineup = racingGame.setLineup("car1", "car2");
 
         //then
         assertThat(lineup.numberOfCars()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("경주 게임은 중복된 이름의 자동차를 만들 수 없다")
+    void duplicate_car_name() {
+        //given
+        RacingGame racingGame = new RacingGame();
+
+        //when
+        Throwable throwable = catchThrowable(() -> racingGame.setLineup("car", "car"));
+
+        //then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
     }
 }
